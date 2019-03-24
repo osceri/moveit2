5c6f7a9f4a0bead142b49807570d43861d2e616d
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2019-03-24 11:20:50 +0100

    Add additional logic required to compile in OS X

--- osx-compile.sh
+++ osx-compile.sh
@@ -20,4 +20,7 @@ export OPENSSL_ROOT_DIR="/usr/local/opt/openssl"
 #Ignore packages
 touch src/image_common/camera_calibration_parsers/COLCON_IGNORE
 touch src/image_common/camera_info_manager/COLCON_IGNORE
-colcon build --merge-install
+export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/usr/local/opt/qt
+export PATH=$PATH:/usr/local/opt/qt/bin
+export LIBRARY_PATH="/usr/local/opt/tinyxml2/lib/"
+colcon build --merge-install --cmake-args -DOpenMP_C_LIB_NAMES="omp" -DOpenMP_CXX_LIB_NAMES="omp" -DOpenMP_C_FLAGS="-Xpreprocessor -fopenmp -lomp -I/usr/local/opt/libomp/include" -DOpenMP_CXX_FLAGS="-Xpreprocessor -fopenmp -lomp -I/usr/local/opt/libomp/include" -DOpenMP_omp_LIBRARY="/usr/local/opt/libomp/lib/libomp.dylib"
