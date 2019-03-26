5cf91f2396ed8ab9ff43bee844ecacf14647e4fb
Lander Usategui San Juan <lander.usategui@gmail.com>
2019-03-26 16:51:14 +0100

    Update docker-entrypoint.sh
    --cmake-args -DBUILD_TESTING=OFF
    
    Avoid: 
    ```bash
    /root/ws_moveit/src/image_common/image_transport/test/test_remapping.cpp:22:13: error: ‘NodeOptions’ is not a member of ‘rclcpp’
    rclcpp::NodeOptions node_options;
    ```
    
    Introduced at: https://github.com/ros-perception/image_common/commit/b86edf5723e0a4ab15f770c685a6001717f84205

--- .docker/ci/docker-entrypoint.sh
+++ .docker/ci/docker-entrypoint.sh
@@ -5,4 +5,4 @@ set -e
 /bin/bash -c "find /opt/ros/crystal/ -name tf2_eigen | xargs rm -rf && source /opt/ros/crystal/setup.bash \
 	       && touch /root/ws_moveit/src/image_common/camera_calibration_parsers/COLCON_IGNORE \
               && touch /root/ws_moveit/src/image_common/camera_info_manager/COLCON_IGNORE \
-              && colcon build --merge-install"
+              && colcon build --merge-install --cmake-args -DBUILD_TESTING=OFF"
