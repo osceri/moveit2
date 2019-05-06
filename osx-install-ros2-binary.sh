9c94fc1a57176f932f30cf036be2bac42b3d7a6c
ahcorde <ahcorde@gmail.com>
2019-05-06 18:37:17 +0200

    resource retriever from soruces

--- osx-install-ros2-binary.sh
+++ osx-install-ros2-binary.sh
@@ -10,3 +10,4 @@ wget https://github.com/ros2/ros2/releases/download/release-crystal-20190408/ros
 tar jxf ros2-crystal-20190408-macos-amd64.tar.bz2
 # Remove tf2_eigen
 find ros2-osx/ -name tf2_eigen | xargs rm -rf
+find ros2-osx/ -name resource_retriever | xargs rm -rf
